1d9c99c1d3b6de2e5695bcdb762c6cea89e512a7
Vince Williams <williamsvm20@gmail.com>
2020-07-24 12:15:50 -0400

    Login is working and pushing to private Route "/bubbles"

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblesPage from './components/BubblePage'
+import PrivateRoute from './utils/PrivateRoute'
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+
+        <PrivateRoute path='/bubbles' >
+          <BubblesPage />
+        </PrivateRoute>
+
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const initialFormValues = {
+    username: '',
+    password: ''
+  }
+
+  const [formValues, setFormValues ] = useState(initialFormValues)
+  const { push } = useHistory()
+
+  const onChange = event => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const onSubmit = event => {
+    event.preventDefault()
+
+    axios.post('http://localhost:5000/api/login', formValues)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+        push('/bubbles')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={onSubmit} >
+        <label>Username:
+          <input 
+            type='text'
+            name='username'
+            value={formValues.username}
+            onChange={onChange}
+          />
+        </label>
+        <label>Password:
+          <input 
+            type='password'
+            name='password'
+            value={formValues.password}
+            onChange={onChange}
+          />
+        </label>
+        <button>Login</button>
+      </form>
     </>
   );
 };
